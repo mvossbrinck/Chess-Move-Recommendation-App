66c010c267adb84714606b32242f93478fb4c419
Madeline Vossbrinck <madeline.vossbrinck@gmail.com>
2021-03-21 23:23:01 -0400

    Clean up code

---
+++ chess_app/chess_neuralnet.py
@@ -0,0 +1,73 @@
+import chess
+from keras.models import Sequential, load_model, model_from_json
+
+
+def make_matrix(board):
+    pgn = board.epd()
+    foo = []
+    pieces = pgn.split(" ", 1)[0]
+    rows = pieces.split("/")
+    for row in rows:
+        foo2 = []
+        for thing in row:
+            if thing.isdigit():
+                for i in range(0, int(thing)):
+                    foo2.append('.')
+            else:
+                foo2.append(thing)
+        foo.append(foo2)
+    return foo
+
+def translate(matrix,chess_dict):
+    rows = []
+    for row in matrix:
+        terms = []
+        for term in row:
+            terms.append(chess_dict[term])
+        rows.append(terms)
+    return rows
+
+chess_dict = {
+    'p' : [1,0,0,0,0,0,0,0,0,0,0,0],
+    'P' : [0,0,0,0,0,0,1,0,0,0,0,0],
+    'n' : [0,1,0,0,0,0,0,0,0,0,0,0],
+    'N' : [0,0,0,0,0,0,0,1,0,0,0,0],
+    'b' : [0,0,1,0,0,0,0,0,0,0,0,0],
+    'B' : [0,0,0,0,0,0,0,0,1,0,0,0],
+    'r' : [0,0,0,1,0,0,0,0,0,0,0,0],
+    'R' : [0,0,0,0,0,0,0,0,0,1,0,0],
+    'q' : [0,0,0,0,1,0,0,0,0,0,0,0],
+    'Q' : [0,0,0,0,0,0,0,0,0,0,1,0],
+    'k' : [0,0,0,0,0,1,0,0,0,0,0,0],
+    'K' : [0,0,0,0,0,0,0,0,0,0,0,1],
+    '.' : [0,0,0,0,0,0,0,0,0,0,0,0],
+}
+
+def evaluate(fen):
+    json_file = open('chess_best_model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    loaded_model.load_weights("chess_best_model.h5")
+    lst = []
+    board = chess.Board(fen)
+    for move in board.legal_moves:
+            board.push(move)
+            matrix = make_matrix(board)
+            board.pop()
+            rows = translate(matrix,chess_dict)
+            value = (loaded_model.predict([rows])).item()
+            move_san = board.san(move)
+            item = [move_san, value]
+            lst.append(item)
+    return lst
+
+def get_NN_move(fen):
+    evaluation = evaluate(fen)
+    maximum = -1
+    best_move = None
+    for term in evaluation:
+        if term[1] > maximum:
+            maximum = term[1]
+            best_move = term[0]
+    return best_move
--- chess_app/hello.py
+++ chess_app/hello.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from chess_random import get_random_move
 from chess_minimax2 import get_minimax_move
+from chess_neuralnet import get_NN_move
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,8 +15,10 @@ def fen_output():
     request_data = request.get_json()
     if (request_data['algorithm'] == 'Random Move'):
         nextMove = get_random_move(request_data["currentBoardPosition"])
-    else:
+    elif (request_data['algorithm'] == 'Min Max'):
         nextMove = get_minimax_move(request_data["currentBoardPosition"])
+    else:
+        nextMove = get_NN_move(request_data["currentBoardPosition"])
     return {'nextMove': nextMove, 'algorithm': request_data['algorithm']}
 
 
